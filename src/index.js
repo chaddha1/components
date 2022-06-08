5bb1862200c9d7a60137aa41c9a667c2ffc11085
chaddha1 <himanshuchaddha4u@gmail.com>
2022-06-08 11:38:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style/App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CommentDetail from "./CommentDetail";
+import { faker } from "@faker-js/faker";
+import ApprovalCard from "./ApprovalCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+    <ApprovalCard>
+        <div>
+            <h4>Warning!</h4>
+            You want to delete this item, Are you sure ?
+        </div>
+    </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          text="Nice blog post!"
+          imageURL={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          text="I like blog post!"
+          imageURL={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          text="Nice subject of the post!"
+          imageURL={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
